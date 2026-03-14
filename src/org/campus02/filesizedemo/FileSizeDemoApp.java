f865a8dc771d6e71ab7bd9648dbe1c52cb4831c5
stephan.kochauf@gmail.com <stephan.kochauf@gmail.com>
2026-03-14 08:45:24 +0100

    Final Implementation with Rekursion

--- src/org/campus02/filesizedemo/FileSizeDemoApp.java
+++ src/org/campus02/filesizedemo/FileSizeDemoApp.java
@@ -14,17 +14,25 @@ public class FileSizeDemoApp {
         * \\ =>
         * */
 
-        File dir = new File("D:\\temp\\ETS26\\sub-ordner\\dokumente");
+        File rootDirectory = new File("D:\\temp\\ETS26\\sub-ordner\\dokumente");
+        long totalFileSizes = getFileSizes(rootDirectory);
 
-        long size = 0;
-        for (File f : dir.listFiles()) {
+        System.out.println("size = " + totalFileSizes);
+        System.out.println("size [KB] = " + totalFileSizes / 1024.0);
+    }
+
+    private static long getFileSizes(File rootDirectory) {
+        long totalFileSizes = 0;
+        for (File f : rootDirectory.listFiles()) {
             System.out.println(f.getName());
             if (f.isFile()) {
-                size = size + f.length();
+                totalFileSizes = totalFileSizes + f.length();
+            }
+            else {
+                // zwangsläufig ein Directory
+                totalFileSizes += getFileSizes(f); // Rekursion
             }
         }
-
-        System.out.println("size = " + size);
-        System.out.println("size [KB] = " + size / 1024.0);
+        return totalFileSizes;
     }
 }
