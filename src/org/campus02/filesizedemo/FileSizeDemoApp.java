460ce9c658b946201ed5538712ac64e75054639b
stephan.kochauf@gmail.com <stephan.kochauf@gmail.com>
2026-03-14 08:36:27 +0100

    Implementation of Dir-Size Calculations

--- src/org/campus02/filesizedemo/FileSizeDemoApp.java
+++ src/org/campus02/filesizedemo/FileSizeDemoApp.java
@@ -1,7 +1,30 @@
 package org.campus02.filesizedemo;
 
+import java.io.File;
+
 public class FileSizeDemoApp {
     public static void main(String[] args) {
+        //D:\temp\ETS26\sub-ordner\dokumente
 
+        /*
+        * \ => Escape
+        * \t => Tabulator
+        * \r => return
+        * \n => line feed
+        * \\ =>
+        * */
+
+        File dir = new File("D:\\temp\\ETS26\\sub-ordner\\dokumente");
+
+        long size = 0;
+        for (File f : dir.listFiles()) {
+            System.out.println(f.getName());
+            if (f.isFile()) {
+                size = size + f.length();
+            }
+        }
+
+        System.out.println("size = " + size);
+        System.out.println("size [KB] = " + size / 1024.0);
     }
 }
